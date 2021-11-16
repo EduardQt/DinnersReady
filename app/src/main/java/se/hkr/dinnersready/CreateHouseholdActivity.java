fe7dd66ca828e281029418fd0f4dbfd40b9f7560
petar2004 <57756792+petar2004@users.noreply.github.com>
2021-11-16 20:00:16 +0100

    Error handling
    Error handling for CreateHouseholdActivity

--- app/src/main/java/se/hkr/dinnersready/CreateHouseholdActivity.java
+++ app/src/main/java/se/hkr/dinnersready/CreateHouseholdActivity.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class CreateHouseholdActivity extends AppCompatActivity {
+public class CreateHouseholdActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText textHouseholdName, textHouseholdPassword , textHouseholdConfirmPassword;
+    private EditText textHouseholdName, textHouseholdPassword, textHouseholdConfirmPassword;
     private Button createHousehold;
 
     @Override
@@ -21,5 +21,50 @@ public class CreateHouseholdActivity extends AppCompatActivity {
         textHouseholdPassword = (EditText) findViewById(R.id.editTextHouseholdPassword);
         textHouseholdConfirmPassword = (EditText) findViewById(R.id.editTextHouseholdPasswordConfirm);
         createHousehold = (Button) findViewById(R.id.buttonCreateHousehold);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.buttonCreateHousehold:
+                creation();
+                break;
+        }
+    }
+
+    public void creation() {
+        String householdName = textHouseholdName.getText().toString().trim();
+        String householdPassword = textHouseholdPassword.getText().toString().trim();
+        String householdConfirmPassword = textHouseholdConfirmPassword.getText().toString().trim();
+
+        if (householdName.isEmpty()) {
+            textHouseholdName.setError("Household name is required");
+            textHouseholdName.requestFocus();
+            return;
+        }
+
+        if (householdPassword.isEmpty()) {
+            textHouseholdPassword.setError("Password is required");
+            textHouseholdPassword.requestFocus();
+            return;
+        }
+
+        if (householdPassword.length() < 6) {
+            textHouseholdPassword.setError("Password needs to be longer than 6 characters");
+            textHouseholdPassword.requestFocus();
+            return;
+        }
+
+        if (householdConfirmPassword.isEmpty()) {
+            textHouseholdConfirmPassword.setError("You must confirm the password");
+            textHouseholdConfirmPassword.requestFocus();
+            return;
+        }
+
+        if (!householdConfirmPassword.equals(householdPassword)) {
+            textHouseholdConfirmPassword.setError("Passwords must match");
+            textHouseholdConfirmPassword.requestFocus();
+            return;
+        }
     }
 }
